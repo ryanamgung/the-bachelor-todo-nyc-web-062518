005f840e7de1009b94bbf14986877f2ff0216c74
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 01:46:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,11 +51,7 @@ def get_average_age_for_season(data, season)
   counter = 0
   data.each do |season_num, hash|
     hash.each do |outcome|
-      outcome.each do |key, value|
-        if key == "age"
-          total_count << value.to_i
-        end
-      end
+      
     end
   end
   total_number = 0
