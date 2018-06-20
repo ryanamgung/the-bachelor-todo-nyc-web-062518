7b1aacbc3b9fc05c8d307d9f99cad91b3cd48a1d
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 01:29:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,17 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  silly_string = ""
+  data.each do |season_num, hash|
+    if season_num == season
+      hash.each do |outcome|
+        if outcome["status"] == "Winner"
+          silly_string = outcome["name"].split(" ").first
+        end
+      end
+    end
+  end
+  silly_string
 end
 
 def count_contestants_by_hometown(data, hometown)
